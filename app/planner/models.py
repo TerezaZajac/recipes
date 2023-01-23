bd062c215a7f06d39bafb356cee3db9cbf3924e8
Tereza Z <zajacova.tereza@gmail.com>
2023-01-23 19:59:31 +0100

    allow_host

--- app/planner/models.py
+++ app/planner/models.py
@@ -5,20 +5,35 @@ from django.db import models
 class Tag(models.Model):
     label = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.label
+
 class Course(models.Model):
     title = models.CharField(max_length=200)
     label = models.ForeignKey(Tag, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
+
 class Unit(models.Model):
     unit = models.FloatField(default=0)
 
+    def __str__(self):
+        return self.unit
+
 class Ingredience(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 class Recipe(models.Model):
     title = models.ForeignKey(Course, on_delete=models.CASCADE)
     label = models.ForeignKey(Tag, on_delete=models.CASCADE)
     ingredience = models.ForeignKey(Ingredience, on_delete=models.CASCADE)
     instruction = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return self.title
 
 
